ed7fc6fc9645b9e0ca00c70ab52941779f83368c
ulysses316 <ulises316@live.com.mx>
2021-08-29 08:56:04 -0500

    minor bug

--- frontend/js/principal.js
+++ frontend/js/principal.js
@@ -1,6 +1,5 @@
 let one = document.querySelector(".uno");
 let two = document.querySelector(".dos");
-let three =document.querySelector(".tres");
 let four = document.querySelector(".cuatro");
 let five =document.querySelector(".cinco");
 let six = document.querySelector(".seis");
@@ -22,7 +21,6 @@ telegram - rawrarawrararaw
 function cambiaTextoTiburon(){
     one.text = "rawrarawraw";
     two.text = "rawrawrararaw";
-    three.text = "rawrarawrararaw";
     four.innerHTML = "Rawrawrara rawra rawrawrararaw ";
     five.innerHTML = "raw rawra rawrawrawrawrraw ra ra raw raw raw raw raw ra rarara raw rawraw rawra rawrawrawrawrraw ra ra raw raw raw raw rawraw raw";
     six.innerHTML = "rawraw raw rawrarawraw rara rawrawrararaw ";
@@ -38,7 +36,6 @@ function cambiaTextoTiburon(){
 function cambiaTextoHumano(){
     one.text = "human";
     two.text = "shark";
-    three.text = "telegram";
     four.innerHTML = "Talk with a shark";
     five.innerHTML = "Are you tired of not understanding your shark friend. Don't worry! We created an app that will allow you to communicate with your friend. It's a shark-translator. Just record a voice note and we will translate it to shark. And there's more. Also, a shark will be able to translate to human what  they is trying to say. ";
     six.innerHTML = "Translate from human a shark.";
