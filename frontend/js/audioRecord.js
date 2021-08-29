fb8954dea44424cbb5f00aa9609bafad959f813e
ulysses316 <ulises316@live.com.mx>
2021-08-29 06:23:10 -0500

    preparacion para enviar el audio, reproducir el audio grabado y preparado para poner el audio convertido, ajuste de estilos

--- frontend/js/audioRecord.js
+++ frontend/js/audioRecord.js
@@ -1,3 +1,17 @@
+let sendData = (audioArchive, url) =>{
+  let formData = new FormData();
+  formData.set('audio', audioArchive);
+
+  fetch(url, {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log("Success:", response))
+}
+
+
 const recordAudio = () =>      
   new Promise(async resolve => {  
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -13,11 +27,17 @@ const recordAudio = () =>
     const stop = () =>         
       new Promise(resolve => {
         mediaRecorder.addEventListener("stop", () => {
-          const audioBlob = new Blob(audioChunks);
+          const audioBlob = new Blob(audioChunks,{ type: 'audio/mpeg-3' });
+          // sendData(audioBlob)
           const audioUrl = URL.createObjectURL(audioBlob); 
           const audio = new Audio(audioUrl);
           const play = () => audio.play();
           resolve({ audioBlob, audioUrl, play });
+          //Put audio on audio tag
+          document.querySelector(".original-audio").src=audioUrl;
+          document.querySelector(".original-audio").controls=true;
+          document.querySelector(".titulo-audio-original").style.display = "block"
+          document.querySelector("#particles-js").style.height = "150%"
         });
 
         mediaRecorder.stop();
@@ -39,6 +59,5 @@ const handleAction = async () => {
 
 const handleActionStop = async () => {
     const audio = await recorder.stop();
-    audio.play();
-    actionButton.disabled = false;  
+    //actionButton.disabled = false;  
 }
